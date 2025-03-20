adaa6c511b34e6d5fec17d31d708a90f0def92ff
Veaceslav.Cebotari <veaceslav.cebotari@maib.com>
2025-03-20 21:13:51 +0200

    Create table product and added entity and repository

---
+++ infrastructure/persistance/src/main/java/com/transactional/outbox/pattern/entity/ProductEntity.java
@@ -0,0 +1,39 @@
+package com.transactional.outbox.pattern.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "t_product")
+public class ProductEntity {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column
+    private String name;
+
+    @Column
+    private BigDecimal price;
+
+    @Column
+    private String category;
+
+    @Column
+    private Instant createdAt;
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+}
---
+++ infrastructure/persistance/src/main/resources/db/migration/V2__create_table_product.sql
@@ -0,0 +1,8 @@
+CREATE TABLE public.t_product
+(
+    id         UUID PRIMARY KEY,
+    price      NUMERIC(16, 2) NOT NULL,
+    created_at TIMESTAMP      NOT NULL,
+    category   VARCHAR(50)    NOT NULL,
+    name       VARCHAR(100)   NOT NULL
+);
